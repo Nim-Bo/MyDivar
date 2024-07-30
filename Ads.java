638e98f077b0bb4e0592903b95805f91de712636
Nim-Bo <Boghrat83@gmail.com>
2024-07-31 02:13:27 +0330

    Add Faze 1 Structure

---
+++ Ads.java
@@ -0,0 +1,5 @@
+public interface Ads {
+    public void add_advertise(String username, String title);
+    public void rem_advertise(String username, String title);
+    public void list_my_advertises(String username);
+}
---
+++ User.java
@@ -0,0 +1,20 @@
+public class User implements Ads {
+    public void register(String username) {
+
+    }
+
+    @Override
+    public void add_advertise(String username, String title) {
+
+    }
+
+    @Override
+    public void rem_advertise(String username, String title) {
+
+    }
+
+    @Override
+    public void list_my_advertises(String username) {
+
+    }
+}
